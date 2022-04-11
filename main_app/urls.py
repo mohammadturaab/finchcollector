08a86b37798718739e96ab2e68ed1683e2cb7ab6
mohammadturaab <mohammadturaab@gmail.com>
2022-04-11 17:32:38 -0500

    upated url.py error

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('teams/', views.NBAList.as_view(), name='nbalist'),
     path('teams/new/', views.NBA_Create.as_view(), name="nba_create"),
     path('teams/<int:pk>/', views.NBADetail.as_view(), name="nba_detail"),
-    path('cats/<int:pk>/update/', views.NBAUpdate.as_view(), name="nba_update"),
-    path('cats/<int:pk>/delete/', views.NBADelete.as_view(), name="nba_delete"),
+    path('teams/<int:pk>/update/', views.NBAUpdate.as_view(), name="nba_update"),
+    path('teams/<int:pk>/delete/', views.NBADelete.as_view(), name="nba_delete"),
     path('user/<username>/', views.profile, name='profile'),
 
 ]
