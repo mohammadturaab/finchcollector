519da3876e98c10a2977aebbd2cb040a46744c9e
mohammadturaab <mohammadturaab@gmail.com>
2022-04-12 19:58:40 -0500

    added players model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+POSITIONS = (
+    ('pg', 'point guard'),
+    ('sg', 'shooting guard'),
+    ('sf', 'small forward'),
+    ('pf', 'power forward'),
+    ('c', 'center')
+)
+class Players(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=50, choices=POSITIONS)
+
+    def __str__(self):
+        return self.name
+
 class NBA(models.Model):
     name = models.CharField(max_length=50)
     img = models.CharField(max_length=250)
